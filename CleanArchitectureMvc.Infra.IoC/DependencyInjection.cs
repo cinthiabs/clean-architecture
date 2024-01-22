9abefc95b25e2201ad073d953b002a6198bb110c
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-01-22 20:53:02 -0300

    Ioc

---
+++ CleanArchitectureMvc.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitectureMvc.Infra.Data.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CleanArchitectureMvc.Domain.Interfaces;
+using CleanArchitectureMvc.Infra.Data.Repositories;
+
+
+
+namespace CleanArchitectureMvc.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+                ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            return services;
+
+        }
+    }
+}
