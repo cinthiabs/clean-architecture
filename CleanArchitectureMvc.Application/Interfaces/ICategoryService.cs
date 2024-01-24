0022680aeb0cebdecbe821c69601e189b155f7d7
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-01-23 21:36:15 -0300

    mapper

---
+++ CleanArchitectureMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitectureMvc.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureMvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDTO);
+        Task Update(CategoryDTO categoryDTO);
+        Task Remove(int? id);
+
+    }
+}
---
+++ CleanArchitectureMvc.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CleanArchitectureMvc.Application.DTOs;
+using CleanArchitectureMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureMvc.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
