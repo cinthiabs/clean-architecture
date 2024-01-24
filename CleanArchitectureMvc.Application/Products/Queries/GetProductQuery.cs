06265df9da32e7abced790bc1767d7ce449b91a7
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-01-24 07:54:30 -0300

    CQRS

---
+++ CleanArchitectureMvc.Application/Products/Queries/GetProductQuery.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitectureMvc.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureMvc.Application.Products.Queries
+{
+    public class GetProductQuery : IRequest<IEnumerable<Product>>
+    {
+    }
+}
