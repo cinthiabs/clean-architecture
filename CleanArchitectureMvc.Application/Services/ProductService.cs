7b592bcff3ce5e7ad92afa5b29ba6f04e589c8f2
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-01-23 21:51:39 -0300

    mapper

---
+++ CleanArchitectureMvc.Application/Services/ProductService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using CleanArchitectureMvc.Application.DTOs;
+using CleanArchitectureMvc.Application.Interfaces;
+using CleanArchitectureMvc.Domain.Entities;
+using CleanArchitectureMvc.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+        private IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Add(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.CreateAsync(productEntity);
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+            var productEntity = await _productRepository.GetByIdAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productsEntity = await _productRepository.GetProductsAsync();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var productEntity = _productRepository.GetByIdAsync(id).Result;
+            await _productRepository.RemoveAsync(productEntity);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.UpdateAsync(productEntity);
+        }
+    }
+}
